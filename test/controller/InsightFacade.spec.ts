4644bb791807281487eac68ee94186b50e54a464
amehra123 <60304331+amehra123@users.noreply.github.com>
2023-01-17 00:30:44 -0800

    started doing tests

---
+++ test/controller/InsightFacade.spec.ts
@@ -0,0 +1,63 @@
+import InsightFacade from "../../src/controller/InsightFacade";
+import {clearDisk, getContentFromArchives} from "../resources/archives/TestUtil";
+import {InsightDatasetKind, InsightError} from "../../src/controller/IInsightFacade";
+import chai, {expect} from "chai";
+import chaiAsPromised = require("chai-as-promised");
+
+chai.use(chaiAsPromised);
+
+describe("InsightFacade", function()  {
+    let sections: string;
+    let facade: InsightFacade;
+
+    before(function() {
+        sections = getContentFromArchives("pair.zip");
+    });
+
+    beforeEach(function() {
+         clearDisk()
+         facade = new InsightFacade();
+    });
+
+    describe("addDataset", function() {
+
+        it ("should reject with  an empty dataset id", function() {
+            const result = facade.addDataset("", sections, InsightDatasetKind.Sections)
+
+            return expect(result).to.eventually.be.rejectedWith(InsightError);
+        });
+
+        it ("should reject same name", function(){
+            facade.addDataset("aaa", sections, InsightDatasetKind.Sections)
+                .then((res) => {
+                    facade.addDataset("aaa", sections, InsightDatasetKind.Sections)
+                })
+                //shouldn't go to this then?
+                .then((res) =>{
+                    expect
+                })
+                .catch((err) => {
+
+
+            });
+        });
+
+
+
+        it("Should resolve with a correct dataset", function() {
+            return facade.addDataset("Course", sections, InsightDatasetKind.Sections)
+                .then((res) => {
+                    //result should be the name of the id
+                    expect(res).to.deep.equal(["Course"]);
+                    //should be an array
+                    expect(res).to.be.an.instanceof(Array);
+                    //length should be 1 (only added 1)
+                    expect(res.length).to.equal(1);
+                    })
+                .catch((err) => {
+                    throw new Error("Should not have rejected");
+
+            });
+        });
+    });
+});
