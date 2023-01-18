225d21bf3defe87da608c42824930d87c93147fa
amehra123 <60304331+amehra123@users.noreply.github.com>
2023-01-17 17:20:04 -0800

    Added more base tests

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -27,28 +27,42 @@ describe("InsightFacade", function()  {
             return expect(result).to.eventually.be.rejectedWith(InsightError);
         });
 
-        /*it ("should reject same name", function(){
-            facade.addDataset("aaa", sections, InsightDatasetKind.Sections)
-                .then((res) => {
+        it ("should reject same name", function(){
+             const result = facade.addDataset("aaa", sections, InsightDatasetKind.Sections)
+                .then(() => {
                     facade.addDataset("aaa", sections, InsightDatasetKind.Sections)
                 })
-                //shouldn't go to this then?
-                .then((res) =>{
-                    expect
-                })
-                .catch((err) => {
-
-
-            });
+                return expect(result).to.eventually.be.rejectedWith(InsightError);
         });
-        */
+
+
+        it("should reject name with underscore", function() {
+            const result = facade.addDataset("a_b", sections, InsightDatasetKind.Sections)
+            return expect(result).to.eventually.be.rejectedWith(InsightError);
+        });
+
+        it("should reject name with only whitespace", function() {
+            const result = facade.addDataset("  ", sections, InsightDatasetKind.Sections)
+            return expect(result).to.eventually.be.rejectedWith(InsightError);
+        });
+
+        it("should reject content that is not a zip file", function() {
+            const result = facade.addDataset("test", "Randomstringggg", InsightDatasetKind.Sections)
+            return expect(result).to.eventually.be.rejectedWith(InsightError);
+        });
+
+        it("should reject the wrong InsightDatasetKind ", function() {
+            const result = facade.addDataset("test", sections, InsightDatasetKind.Rooms)
+            return expect(result).to.eventually.be.rejectedWith(InsightError);
+        });
+
 
 
         it("Should resolve with a correct dataset", function() {
-            return facade.addDataset("Course", sections, InsightDatasetKind.Sections)
+            return facade.addDataset("testing", sections, InsightDatasetKind.Sections)
                 .then((res) => {
                     //result should be the name of the id
-                    expect(res).to.deep.equal(["Course"]);
+                    expect(res).to.deep.equal(["testing"]);
                     //should be an array
                     expect(res).to.be.an.instanceof(Array);
                     //length should be 1 (only added 1 dataset)
